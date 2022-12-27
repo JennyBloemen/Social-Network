0b31514d7c9561e6e25e3046967d298c428afdb9
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-27 15:05:42 -0700

    wrote thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+
+const {
+  getThought,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thought-controller");
+const { create } = require("../../models/user");
+
+router.route("/").get(getThought).post(createThought);
+
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(createReaction);
+
+router.route("/:thought/reactions/:reactionsId").delete(deleteReaction);
+
+module.exports = router;
